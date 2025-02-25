49957ba0f90e922d584e73c66f209727b4e9c0f9
maikeleckelboom <maikeleckelboom@outlook.com>
2025-02-25 04:53:28 +0100

    chore: change browser test

--- test/pixelift.browser.test.ts
+++ test/pixelift.browser.test.ts
@@ -1,13 +1,19 @@
 import { describe, expect, it } from 'vitest'
 import { pixelift } from '../src/browser'
 
+const externalUrls = {
+  png: 'https://upload.wikimedia.org/wikipedia/commons/4/47/PNG_transparency_demonstration_1.png',
+  jpg: 'https://upload.wikimedia.org/wikipedia/commons/d/df/Close_wing_Basking_of_Athyma_perius_%28Linnaeus%2C_1758%29_-_Common_Sergeant_%284%29_WLB.jpg',
+  gif: 'https://upload.wikimedia.org/wikipedia/commons/2/2c/Rotating_earth_%28large%29.gif',
+  webp: 'https://upload.wikimedia.org/wikipedia/commons/b/bc/Free-Instagram.webp'
+} as const
+
 describe('Browser Environment', () => {
   it('should run in a browser environment', () => {
     expect(window).toBeDefined()
   })
 
   it('decodes PNG correctly', async () => {
-    // const externalUrl = 'https://upload.wikimedia.org/wikipedia/commons/4/47/PNG_transparency_demonstration_1.png'
     const url = new URL('./assets/test.png', import.meta.url)
     const { data, width, height, channels } = await pixelift(url)
     expect(width).toBeDefined()
@@ -17,7 +23,6 @@ describe('Browser Environment', () => {
   })
 
   it('handles 3-channel JPG', async () => {
-    // const externalUrl = 'https://upload.wikimedia.org/wikipedia/commons/d/df/Close_wing_Basking_of_Athyma_perius_%28Linnaeus%2C_1758%29_-_Common_Sergeant_%284%29_WLB.jpg'
     const url = new URL('./assets/test.jpg', import.meta.url)
     const { data, width, height, channels } = await pixelift(url)
     expect(width).toBeDefined()
@@ -27,7 +32,6 @@ describe('Browser Environment', () => {
   })
 
   it('decodes GIF correctly', async () => {
-    // const externalUrl = 'https://upload.wikimedia.org/wikipedia/commons/2/2c/Rotating_earth_%28large%29.gif'
     const url = new URL('./assets/test.gif', import.meta.url)
     const { data, width, height, channels } = await pixelift(url)
     expect(width).toBeDefined()
@@ -37,7 +41,6 @@ describe('Browser Environment', () => {
   })
 
   it('decodes WebP correctly', async () => {
-    // const externalUrl = 'https://upload.wikimedia.org/wikipedia/commons/b/bc/Free-Instagram.webp'
     const url = new URL('./assets/test.webp', import.meta.url)
     const { data, width, height, channels } = await pixelift(url)
     expect(width).toBeDefined()
